3c34f0380d358f0dac6e4af841d76dcf50499a31
dongyeop <hava12@naver.com>
2022-04-26 22:41:11 +0900

    1. 기본 Job, Step 생성 2. h2 데이터베이스 연동

---
+++ src/main/java/com/example/jarinara_schedular/config/JarinaraConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.jarinara_schedular.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class JarinaraConfiguration {
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job jarinaraJob() {
+		return jobBuilderFactory.get("jarinaraJob")
+			.start(jarinaraStep())
+			.next(jarinaraStep2())
+			.build();
+	}
+
+	@Bean
+	public Step jarinaraStep2() {
+		return stepBuilderFactory.get("jarinaraStep2")
+			.tasklet(((contribution, chunkContext) -> {
+
+				System.out.println("Hello Spring Batch2");
+				return RepeatStatus.FINISHED;
+			})).build();
+	}
+
+	@Bean
+	public Step jarinaraStep() {
+		return stepBuilderFactory.get("jarinaraStep")
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("Hello Spring Batch");
+				return RepeatStatus.FINISHED;
+			}).build();
+	}
+}
